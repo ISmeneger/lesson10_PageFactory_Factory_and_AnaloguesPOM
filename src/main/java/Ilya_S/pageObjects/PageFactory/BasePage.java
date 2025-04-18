297dca895d529a93004e25229246b4801541c341
ISmeneger <ISmeneger@yandex.ru>
2025-04-18 20:20:39 +0300

    Add PageFactory pages

---
+++ src/main/java/Ilya_S/pageObjects/PageFactory/BasePage.java
@@ -0,0 +1,32 @@
+package Ilya_S.pageObjects.PageFactory;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+
+    WebDriver driver;
+
+    //Locators
+    @FindBy(className = "display-6")
+    private WebElement title;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @Step("Getting current url")
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+
+
+    @Step("Get subpage title")
+    public String getTitle() {
+        return title.getText();
+    }
+}
---
+++ src/main/java/Ilya_S/pageObjects/PageFactory/NavigationPage.java
@@ -0,0 +1,29 @@
+package Ilya_S.pageObjects.PageFactory;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class NavigationPage extends BasePage {
+    private static final String NAV_FORM_URL = "navigation1.html";
+
+    @FindBy(linkText = "Next")
+    private WebElement nextButton;
+
+    public NavigationPage(WebDriver driver) {
+        super(driver);
+//        PageFactory.initElements(driver, this);
+    }
+
+    @Step("Get subpage url")
+    public String getUrl() {
+        return NAV_FORM_URL;
+    }
+
+    @Step("Click next")
+    public void clickNextButton() {
+        nextButton.click();
+    }
+}
